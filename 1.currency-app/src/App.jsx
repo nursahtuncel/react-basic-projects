10447f79c24070aa5d7c534fd293265f4466ec03
nursahtuncel <nursahtuncell@gmail.com>
2026-01-03 02:41:04 +0300

    initial commit

---
+++ 1.currency-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Currency from './components/Currency'
+
+const App = () => {
+  return (
+    <div>
+    <Currency />
+    </div>
+  )
+}
+
+export default App
